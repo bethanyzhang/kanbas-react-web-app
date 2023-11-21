f7d19a46cb61bb2e24bae9528ad2e6bc2fecd583
Dan Zhang <zhang.dan4@northeastern.edu>
2023-11-20 20:51:40 -0800

    a5

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,114 +1,169 @@
-import React, { useState } from "react"
+// import React, { useState } from "react"
+// import { useParams } from "react-router-dom"
+// import db from "../../Database"
+// import "./index.css"
+// import { useSelector, useDispatch } from "react-redux"
+// import {
+//   addModule,
+//   deleteModule,
+//   updateModule,
+//   setModule,
+// } from "./modulesReducer"
+
+// function ModuleList () {
+//   const { courseId } = useParams()
+//   const modules = useSelector((state) => state.modulesReducer.modules)
+//   const module = useSelector((state) => state.modulesReducer.module)
+//   const dispatch = useDispatch()
+
+
+
+
+
+//   return (
+//     <ul className="list-group">
+//       <li className="list-group-item">
+//         <button onClick={() => dispatch(addModule({ ...module, course: courseId }))} className="btn btn-success float-end">Add</button>
+//         <button onClick={() => dispatch(updateModule(module))} className="btn btn-primary float-end" style={{ marginRight: 10 }}>
+//           Update
+//         </button>
+
+//         <ul className="list-group" style={{ listStyle: "none" }}>
+//           <li><input value={module.name}
+//             onChange={(e) =>
+//               dispatch(setModule({ ...module, name: e.target.value }))
+//             }
+//             style={{ width: 500, height: 50 }}
+//           /></li>
+//           <li><textarea value={module.description}
+//             onChange={(e) =>
+//               dispatch(setModule({ ...module, description: e.target.value }))
+//             }
+//             style={{ width: 500, height: 100 }}
+//           /></li>
+//         </ul>
+//       </li>
+
+
+//       {
+//         modules
+//           .filter((module) => module.course === courseId)
+//           .map((module, index) => (
+//             <li key={index} className="list-group-item">
+//               <button
+//                 onClick={() => dispatch(setModule(module))} className="btn btn-success float-end">
+//                 Edit
+//               </button>
+
+//               <button
+//                 onClick={() => dispatch(deleteModule(module._id))} className="btn btn-danger float-end" style={{ marginRight: 10 }}>
+//                 Delete
+//               </button>
+
+//               <h3 className="heading">Week {index}</h3>
+//               <h3>{module.name}</h3>
+//               <p>{module.description}</p>
+
+
+
+
+
+
+//             </li>
+//           ))
+//       }
+//     </ul>
+
+//   )
+// }
+
+// export default ModuleList
+
+import React, { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-import db from "../../Database"
-import "./index.css"
-import { useSelector, useDispatch } from "react-redux"
-import {
-  addModule,
-  deleteModule,
-  updateModule,
-  setModule,
-} from "./modulesReducer"
+import * as client from "./client"
 
 function ModuleList () {
   const { courseId } = useParams()
-  const modules = useSelector((state) => state.modulesReducer.modules)
-  const module = useSelector((state) => state.modulesReducer.module)
-  const dispatch = useDispatch()
-  // const [modules, setModules] = useState(db.modules)
-  // const [module, setModule] = useState({
-  //   name: "New Module",
-  //   description: "New Description",
-  //   course: courseId,
-  // })
-  // const addModule = (module) => {
-  //   setModules([
-  //     { ...module, _id: new Date().getTime().toString() },
-  //     ...modules,
-  //   ])
-  // }
-  // const deleteModule = (moduleId) => {
-  //   setModules(modules.filter(
-  //     (module) => module._id !== moduleId))
-  // }
-  // const updateModule = () => {
-  //   setModules(
-  //     modules.map((m) => {
-  //       if (m._id === module._id) {
-  //         return module
-  //       } else {
-  //         return m
-  //       }
-  //     })
-  //   )
-  // }
+  const [modules, setModules] = useState([])
+  const [module, setModule] = useState({})
+  // const modules = db.modules;
+
+  const addModule = async () => {
+    const newModule = await client.addModule(courseId, module)
+    setModules([newModule, ...modules])
+  }
+
+
+  const deleteModule = async (moduleId) => {
+    try {
+      await client.deleteModule(moduleId)
+      setModules(module.filter((module) => module._id == moduleId))
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  const updateModule = async (moduleId, module) => {
+    try {
+      await client.updateModule(moduleId, module)
+      setModules(module.map((c) => (c._id === module._id ? module : c)))
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
 
+  const fetchModules = async () => {
+    const modules = await client.findModulesForCourse(courseId)
+    setModules(modules)
+  }
+
+  useEffect(() => {
+    fetchModules()
+  }, [])
 
   return (
     <ul className="list-group">
       <li className="list-group-item">
-        <button onClick={() => dispatch(addModule({ ...module, course: courseId }))} className="btn btn-success float-end">Add</button>
-        <button onClick={() => dispatch(updateModule(module))} className="btn btn-primary float-end" style={{ marginRight: 10 }}>
+        <button onClick={updateModule} className="btn btn-success">
           Update
         </button>
-
-        <ul className="list-group" style={{ listStyle: "none" }}>
-          <li><input value={module.name}
-            onChange={(e) =>
-              dispatch(setModule({ ...module, name: e.target.value }))
-            }
-            style={{ width: 500, height: 50 }}
-          /></li>
-          <li><textarea value={module.description}
-            onChange={(e) =>
-              dispatch(setModule({ ...module, description: e.target.value }))
-            }
-            style={{ width: 500, height: 100 }}
-          /></li>
-        </ul>
+        <button onClick={addModule} className="btn btn-warning">Add</button>
+        <input
+          value={module.name}
+          onChange={(e) => setModule({ ...module, name: e.target.value })}
+          className="form-control"
+          placeholder="Module Name"
+        />
       </li>
+      {modules
+        .filter((module) => module.course === courseId)
+        .map((module, index) => (
+          <li key={index} className="list-group-item">
+            <h3>{module.name}</h3>
+            <button
+              onClick={() => deleteModule(module._id)}
+              className="btn btn-danger float-end"
+            >
+              Delete
+            </button>
+            <p>{module.description}</p>
+            {module.lessons && (
+              <ul className="list-group">
+                {module.lessons.map((lesson, index) => (
+                  <li key={index} className="list-group-item">
 
-
-      {
-        modules
-          .filter((module) => module.course === courseId)
-          .map((module, index) => (
-            <li key={index} className="list-group-item">
-              <button
-                onClick={() => dispatch(setModule(module))} className="btn btn-success float-end">
-                Edit
-              </button>
-
-              <button
-                onClick={() => dispatch(deleteModule(module._id))} className="btn btn-danger float-end" style={{ marginRight: 10 }}>
-                Delete
-              </button>
-
-              <h3 className="heading">Week {index}</h3>
-              <h3>{module.name}</h3>
-              <p>{module.description}</p>
-
-
-              {/* <ul>
-                {modules
-                  .filter((module) => module.course === courseId)
-                  .map((module, index) => (
-                    <li key={index} className="list-group-item">
-
-
-                      <p>{module.description}</p> </li>))}
-              </ul> */}
-
-
-
-            </li>
-          ))
-      }
+                    <h4>{lesson.name}</h4>
+                    <p>{lesson.description}</p>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
     </ul>
-
   )
 }
-
 export default ModuleList
--- src/Kanbas/Courses/Modules/modulesReducer.js
+++ src/Kanbas/Courses/Modules/modulesReducer.js
@@ -2,16 +2,61 @@ import { createSlice } from "@reduxjs/toolkit"
 import db from "../../Database"
 
 
+// const initialState = {
+//   modules: db.modules,
+//   module: { name: "New Module 123", description: "New Description" },
+// }
+
+
+// const modulesSlice = createSlice({
+//   name: "modules",
+//   initialState,
+//   reducers: {
+//     addModule: (state, action) => {
+//       state.modules = [
+//         { ...action.payload, _id: new Date().getTime().toString() },
+//         ...state.modules,
+//       ]
+//     },
+//     deleteModule: (state, action) => {
+//       state.modules = state.modules.filter(
+//         (module) => module._id !== action.payload
+//       )
+//     },
+//     updateModule: (state, action) => {
+//       state.modules = state.modules.map((module) => {
+//         if (module._id === action.payload._id) {
+//           return action.payload
+//         } else {
+//           return module
+//         }
+//       })
+//     },
+//     setModule: (state, action) => {
+//       state.module = action.payload
+//     },
+//   },
+// })
+
+
+// export const { addModule, deleteModule,
+//   updateModule, setModule } = modulesSlice.actions
+// export default modulesSlice.reducer
+
 const initialState = {
-  modules: db.modules,
-  module: { name: "New Module 123", description: "New Description" },
+  modules: [],
+  module: {
+    name: "New Module 234",
+    description: "New Description"
+  },
 }
-
-
 const modulesSlice = createSlice({
   name: "modules",
   initialState,
   reducers: {
+    setModules: (state, action) => {
+      state.modules = action.payload
+    },
     addModule: (state, action) => {
       state.modules = [
         { ...action.payload, _id: new Date().getTime().toString() },
@@ -37,8 +82,7 @@ const modulesSlice = createSlice({
     },
   },
 })
-
-
 export const { addModule, deleteModule,
-  updateModule, setModule } = modulesSlice.actions
+  updateModule, setModule, setModules } =
+  modulesSlice.actions
 export default modulesSlice.reducer
